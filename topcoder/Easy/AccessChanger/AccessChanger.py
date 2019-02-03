63d3571ab60de2dd7f098756880c3aa7f03f496e
hansololee <arisongha@gmail.com>
2019-02-03 18:08:28 +0900

    add codingtest files

---
+++ topcoder/Easy/AccessChanger/AccessChanger.py
@@ -0,0 +1,31 @@
+
+class AccessChanger:
+    def __init__(self):
+        self.result = []
+        
+    def convert(self,program):
+        cnt=program.count('"')
+        
+        for i,val in enumerate(program):
+            barrier = val.find("//")
+
+            if val.find("->")>-1 and val.find("//")>0:
+                half1 = val[:barrier].replace("->",".")
+                half2 = val[barrier:]
+                val = half1 + half2
+                
+            else : 
+                val = val.replace("->",".")
+            
+            self.result.append(val)
+            print(tuple(self.result))
+
+        return tuple(self.result)
+
+
+
+ac = AccessChanger()
+
+ac.convert(tuple({"->-> // two successive arrows ->->"}))
+
+
