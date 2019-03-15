9d909572ed333ff56b8338714d0555cb235ab12a
hansololee <arisongha@gmail.com>
2019-03-15 13:19:03 +0900

    add new coding test

---
+++ leetcode/Medium/JumpGame/JumpGame.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def __init__(self):
+        self.dic = dict()
+    
+    def canJump(self, nums: 'List[int]') -> bool:
+        if len(nums) == 1:
+            return True
+        
+        self.dic[0] = nums[0]
+        for i in range(1, len(nums)):
+            print(self.dic[i-1], i)
+            if self.dic[i-1] < i:
+                return False
+            
+            self.dic[i] = max(self.dic[i-1], i + nums[i])
+            
+        return self.dic[len(nums)-1] >= len(nums) - 1
+            
+
+sol =  Solution()
+sol.canJump([2,3,1,1,4])
+
