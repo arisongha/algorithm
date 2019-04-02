98c424b18677336e39a878961dd93781fc08aad6
hansololee <arisongha@gmail.com>
2019-04-02 22:16:51 +0900

    add new coding test

---
+++ leetcode/Easy/RansomNote/RansomNote.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in set(ransomNote):
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
+
+
+
+sol = Solution()
+sol.canConstruct("aa", "aab")
+
