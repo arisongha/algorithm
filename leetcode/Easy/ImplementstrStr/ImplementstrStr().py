54320b85b7c3a1560c2bebbfd7699e0dae08c202
hansololee <arisongha@gmail.com>
2019-04-30 15:39:43 +0900

    add new coding test

---
+++ leetcode/Easy/ImplementstrStr/ImplementstrStr().py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+
+
+sol = Solution()
+sol.strStr(haystack = "aaaaa", needle = "bba")
+
