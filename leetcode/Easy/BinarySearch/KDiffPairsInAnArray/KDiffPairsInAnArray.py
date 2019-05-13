85d4dcb9406e1e27f04a57c92bc56422cc3b3b6a
hansololee <arisongha@gmail.com>
2019-05-13 13:48:47 +0900

    add new coding test

---
+++ leetcode/Easy/BinarySearch/KDiffPairsInAnArray/KDiffPairsInAnArray.py
@@ -0,0 +1,23 @@
+
+class Solution:
+    def findPairs(self, nums: 'List[int]', k: int) -> int:
+        if k < 0:
+            return 0
+        
+        result = []
+        for i,v in enumerate(nums):
+            for ind,val in enumerate(nums):
+                cup = []
+                if not i == ind:
+                    if v - val == k:
+                        cup = sorted([v, val])
+                        if not cup in result:
+                            result.append(cup)
+        
+        return len(result)
+
+
+
+sol = Solution()
+sol.findPairs([1, 3, 1, 5, 4], k = 0)
+
