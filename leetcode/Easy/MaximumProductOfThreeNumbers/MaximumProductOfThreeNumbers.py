4dfb744e0841bdc3cb2a8197eef9a3b7ff82cf78
hansololee <arisongha@gmail.com>
2019-07-22 18:10:36 +0900

    add new coding test

---
+++ leetcode/Easy/MaximumProductOfThreeNumbers/MaximumProductOfThreeNumbers.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def maximumProduct(self, nums: 'List[int]') -> int:
+        
+        nums.sort()
+        return max(nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1])
+        
+
+
+sol = Solution()
+sol.maximumProduct([1,2,3,4])
+
