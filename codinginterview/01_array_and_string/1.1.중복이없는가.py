21a57137553c31775e8ec4df05adf6bfe3fc9285
Hansol Lee <44221590+hansololee@users.noreply.github.com>
2019-11-27 16:03:35 +0900

    Update 1.1.중복이없는가.py

--- codinginterview/01_array_and_string/1.1.중복이없는가.py
+++ codinginterview/01_array_and_string/1.1.중복이없는가.py
@@ -1,8 +1,7 @@
-
-# coding: utf-8
-
-# In[2]:
-
+'''
+문자열이 주어졌을 때, 이 문자열에 같은 문자가 중복되어 등장하는지 확인하는 알고리즘을 작성하라. 
+자료구조를 추가로 사용하지 않고 풀 수 있는 알고리즘 또한 고민하라.
+'''
 
 def charDuplicate(string):
     dic = dict()
@@ -11,9 +10,6 @@ def charDuplicate(string):
         if dic[s] > 1:
             return True
     return False
-
-
-# In[6]:
 
 
 charDuplicate("abcdefg")
